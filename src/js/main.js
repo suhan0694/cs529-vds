4e0ea5db6792a88aceb4b80dd006173881bf22cb
Suhan Nath <snath6@uic.edu>
2020-10-19 21:49:00 -0500

    :tada: Changes to make it Module

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,28 @@
-"use strict";
-import Scene from "./scene.js";
-import ParticleSystem from "./particleSystem.js";
+'use strict'
+import Scene from './scene.js'
+import ParticleSystem from './particleSystem.js'
 /* Get or create the application global variable */
-var App = App || {};
+var App = App || {}
 
 /* IIFE to initialize the main entry of the application*/
 
-    // setup the pointer to the scope 'this' variable
-    var self = this;
+// setup the pointer to the scope 'this' variable
+var self = this
 
-    /* Entry point of the application */
-    App.start = function()
-    {
-        // create a new scene, pass options as dictionary
-        App.scene = new Scene({container:"scene"});
+/* Entry point of the application */
+App.start = function () {
+    // create a new scene, pass options as dictionary
+    App.scene = new Scene({ container: 'scene' })
 
-        // initialize the particle system
-        const particleSystem = new ParticleSystem();
-        particleSystem.initialize('data/058.csv');
+    // initialize the particle system
+    const particleSystem = new ParticleSystem()
+    particleSystem.initialize('data/058.csv')
 
-        //add the particle system to the scene
-        App.scene.addObject( particleSystem.getParticleSystems());
+    //add the particle system to the scene
+    App.scene.addObject(particleSystem.getParticleSystems())
 
-        // render the scene
-        App.scene.render();
+    // render the scene
+    App.scene.render()
+}
 
-    };
-
-export default App;
+export default App
--- src/js/particleSystem.js
+++ src/js/particleSystem.js
@@ -1,118 +1,142 @@
 /* author: Andrew Burks */
-"use strict";
-import * as THREE from '../../vendor/three/build/three.module.js';
+'use strict'
+import * as THREE from '../../vendor/three/build/three.module.js'
+import legend from '../../lib/js/legend.js'
 
 /* Get or create the application global variable */
-var App = App || {};
+var App = App || {}
 
-const ParticleSystem = function() {
-
+const ParticleSystem = function () {
     // setup the pointer to the scope 'this' variable
-    const self = this;
+    const self = this
 
     // data container
-    const data = [];
+    const data = []
 
     // scene graph group for the particle system
-    const sceneObject = new THREE.Group();
+    const sceneObject = new THREE.Group()
 
     // bounds of the data
-    const bounds = {};
+    const bounds = {}
 
-    let plane;
+    const margin = { top: 10, right: 30, bottom: 30, left: 60 },
+        width = 460 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom
+
+    const particleColors = d3
+        .scaleSequential(d3.interpolateRgb('maroon', 'midnightblue'))
+        //.domain(d3.extent(data.map(d => d.concentration)));
+        .domain([0, 30].reverse())
+
+    let plane
+    let particles
+    let particleSystem
+
+    let xExtent
+    let yExtent
+
+    let xAxis
+    let yAxis
+
+    let svg
 
     // create the containment box.
     // This cylinder is only to guide development.
     // TODO: Remove after the data has been rendered
-    self.drawContainment = function() {
-
+    self.drawContainment = function () {
         // get the radius and height based on the data bounds
-        const radius = (bounds.maxX - bounds.minX)/2.0 + 1;
-        const height = (bounds.maxY - bounds.minY) + 1;
+        const radius = (bounds.maxX - bounds.minX) / 2.0 + 1
+        const height = bounds.maxY - bounds.minY + 1
 
         // create a cylinder to contain the particle system
-        const geometry = new THREE.CylinderGeometry( radius, radius, height, 32 );
-        const material = new THREE.MeshBasicMaterial( {color: 0xffffff, wireframe: true} );
-        const cylinder = new THREE.Mesh( geometry, material );
+        const geometry = new THREE.CylinderGeometry(radius, radius, height, 32)
+        const material = new THREE.MeshBasicMaterial({
+            color: 0xffffff,
+            wireframe: true,
+        })
+        const cylinder = new THREE.Mesh(geometry, material)
 
         // add the containment to the scene
-        sceneObject.add(cylinder);
-    };
+        sceneObject.add(cylinder)
+    }
 
-    self.drawRectanglePlane = function(){
-        const slider = document.getElementById("zSlider");
-        const valueOutput = document.getElementById("zValue");
-        const sliderRange = d3.scaleLinear()
-                            .domain([0,100])
-                            .range([-5,5])
-        
-        slider.onchange = function(){
-            const value = sliderRange(this.value).toFixed(2);
-            valueOutput.innerHTML = value;
-            plane.position.z = value;
+    self.drawRectanglePlane = function () {
+        const slider = document.getElementById('zSlider')
+        const valueOutput = document.getElementById('zValue')
+        const sliderRange = d3.scaleLinear().domain([1, 100]).range([-5, 5])
+
+        slider.onchange = function () {
+            const value = sliderRange(this.value).toFixed(2)
+            valueOutput.innerHTML = value
+            plane.position.z = value
+            self.drawD3Map(parseInt(value))
+            self.updateForBrush(parseInt(value))
         }
 
-        const width = (bounds.maxX - bounds.minX) + 1;
-        const height = (bounds.maxY - bounds.minY) + 1;
-        const material = new THREE.MeshBasicMaterial( {color: 0xffffff, side: THREE.DoubleSide, transparent: true, opacity: 0.5} );
-        const geometry = new THREE.PlaneGeometry(width,height,2);
-        plane = new THREE.Mesh(geometry, material);
-        plane.translateY(5);
-        sceneObject.add(plane);
+        const width = bounds.maxX - bounds.minX + 1
+        const height = bounds.maxY - bounds.minY + 1
+        const material = new THREE.MeshBasicMaterial({
+            color: 0xffffff,
+            side: THREE.DoubleSide,
+            transparent: true,
+            opacity: 0.5,
+        })
+        const geometry = new THREE.PlaneGeometry(width, height, 2)
+        plane = new THREE.Mesh(geometry, material)
+        plane.translateY(5)
+        sceneObject.add(plane)
     }
 
     // creates the particle system
-    self.createParticleSystem = function() {
+    self.createParticleSystem = function () {
+        const data1 = data.map((d) => d.z)
+        console.log(d3.extent(data1))
 
         // use self.data to create the particle system
         // draw your particle system here!
-        const sprite = new THREE.TextureLoader().load( '../../textures/sprites/disc.png' );
+        const sprite = new THREE.TextureLoader().load(
+            '../../textures/sprites/disc.png'
+        )
 
-        const particleColors = d3.scaleSequential(d3.interpolateRdBu)
-        //.domain(d3.extent(data.map(d => d.concentration)));
-        .domain([0,30].reverse());
-
-        console.log(particleColors(250));
-
-        const particles = new THREE.Geometry();
+        particles = new THREE.Geometry()
         const pMaterial = new THREE.PointsMaterial({
             vertexColors: THREE.VertexColors,
             blending: THREE.AdditiveBlending,
             map: sprite,
             transparent: true,
             depthWrite: false,
-            size: 0.1
-        });
-        let particlePos;
+            size: 0.1,
+        })
+        let particlePos
 
-        for (const particle of data){
+        for (const particle of data) {
             particlePos = new THREE.Vector3(particle.X, particle.Z, particle.Y)
             particles.vertices.push(particlePos)
-            let particleColor = new THREE.Color(particleColors(particle.concentration))
+            let particleColor = new THREE.Color(
+                particleColors(particle.concentration)
+            )
             particles.colors.push(particleColor)
         }
 
-        const particleSystem = new THREE.Points(particles, pMaterial);
+        particleSystem = new THREE.Points(particles, pMaterial)
 
         sceneObject.add(particleSystem)
-
-    };
+    }
 
     // data loading function
-    self.loadData = async function(file){
-
+    self.loadData = async function (file) {
         // read the csv file
-        const loadedFile = await d3.csv(file);
+        const loadedFile = await d3.csv(file)
 
-        loadedFile.forEach(d => { 
-            bounds.minX = Math.min(bounds.minX || Infinity, d.Points0);
-            bounds.minY = Math.min(bounds.minY || Infinity, d.Points1);
-            bounds.minZ = Math.min(bounds.minZ || Infinity, d.Points2);
+        loadedFile.forEach((d) => {
+            bounds.minX = Math.min(bounds.minX || Infinity, d.Points0)
+            bounds.minY = Math.min(bounds.minY || Infinity, d.Points1)
+            bounds.minZ = Math.min(bounds.minZ || Infinity, d.Points2)
 
             // get the max bounds
-            bounds.maxX = Math.max(bounds.maxX || -Infinity, d.Points0);
-            bounds.maxY = Math.max(bounds.maxY || -Infinity, d.Points1);
-            bounds.maxZ = Math.max(bounds.maxY || -Infinity, d.Points2);
+            bounds.maxX = Math.max(bounds.maxX || -Infinity, d.Points0)
+            bounds.maxY = Math.max(bounds.maxY || -Infinity, d.Points1)
+            bounds.maxZ = Math.max(bounds.maxY || -Infinity, d.Points2)
 
             // add the element to the data collection
             data.push({
@@ -125,31 +149,105 @@ const ParticleSystem = function() {
                 // Velocity
                 U: Number(d.velocity0),
                 V: Number(d.velocity1),
-                W: Number(d.velocity2)
-            });
+                W: Number(d.velocity2),
+            })
         })
 
-        self.createParticleSystem();
+        xExtent = d3.extent(data.map((d) => d.X))
+        yExtent = d3.extent(data.map((d) => d.Z))
 
-        self.drawRectanglePlane();
-    };
+        self.createParticleSystem()
+
+        self.drawRectanglePlane()
+
+        self.drawAxis()
+    }
+
+    self.drawD3Map = function (zValue) {
+        const d3Data = data.filter(
+            (d) => d.Y <= zValue + 0.01 && d.Y >= zValue - 0.01
+        )
+        console.log(d3Data)
+
+        d3.select('#scatterPlot').remove()
+
+        svg.append('g')
+            .attr('id', 'scatterPlot')
+            .selectAll('dot')
+            .data(d3Data)
+            .enter()
+            .append('circle')
+            .attr('cx', function (d) {
+                return xAxis(d.X)
+            })
+            .attr('cy', function (d) {
+                return yAxis(d.Z)
+            })
+            .attr('r', 4)
+            .style('fill', (d) => particleColors(d.concentration))
+    }
+
+    self.updateForBrush = function (zValue) {
+        particleSystem.geometry.colorsNeedUpdate = true
+        for (let i = 0; i < data.length; i++) {
+            if (data[i].Y <= zValue + 0.05 && data[i].Y >= zValue - 0.05)
+                particleSystem.geometry.colors[i].set(
+                    particleColors(data[i].concentration)
+                )
+            else
+                particleSystem.geometry.colors[i].set(
+                    d3.scaleSequential(d3.interpolateGreys).domain([0, 30])(
+                        data[i].concentration
+                    )
+                )
+        }
+    }
+
+    self.drawAxis = function () {
+        svg = d3
+            .select('#d3_scatter')
+            .append('svg')
+            .attr('width', width + margin.left + margin.right)
+            .attr('height', height + margin.top + margin.bottom)
+            .append('g')
+            .attr('transform', `translate(${margin.left}, ${margin.top})`)
+
+        xAxis = d3.scaleLinear().domain(xExtent).range([0, width])
+
+        svg.append('g')
+            .attr('transform', 'translate(0,' + height + ')')
+            .call(d3.axisBottom(xAxis))
+
+        // Add Y axis
+        yAxis = d3.scaleLinear().domain(yExtent).range([height, 0])
+        svg.append('g').call(d3.axisLeft(yAxis))
+
+        const d3_legend = document.getElementById('d3_legend')
+        d3_legend.append(
+            legend({
+                color: d3.scaleSequential(
+                    [0, 300],
+                    d3.interpolateRgb('maroon', 'midnightblue')
+                ),
+                title: 'Concentration',
+            })
+        )
+    }
 
     // publicly available functions
     self.public = {
-
         // load the data and setup the system
-        initialize: function(file){
-            self.loadData(file);
+        initialize: function (file) {
+            self.loadData(file)
         },
 
         // accessor for the particle system
-        getParticleSystems : function() {
-            return sceneObject;
-        }
-    };
+        getParticleSystems: function () {
+            return sceneObject
+        },
+    }
 
-    return self.public;
+    return self.public
+}
 
-};
-
-export default ParticleSystem;
+export default ParticleSystem
